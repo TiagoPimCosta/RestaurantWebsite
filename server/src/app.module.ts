9a727ecd2a2e55b616d85f450bd610e66e7bff6c
TiagoPimCosta <tiagopimcosta@gmail.com>
2024-06-01 12:28:37 +0100

    [RW-3] Configure connection to MySql

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfigAsync } from 'config/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync(typeOrmConfigAsync),
+  ],
   controllers: [],
   providers: [],
 })
