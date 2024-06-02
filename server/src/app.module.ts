f21e1df7cef2e2c0d0ab60c88ce19204403020a5
TiagoPimCosta <tiagopimcosta@gmail.com>
2024-06-02 17:31:54 +0100

    [RW-4] CRUD Ingredients

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,13 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfigAsync } from 'config/typeorm.config';
+import { IngredientsModule } from './ingredients/ingredients.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync(typeOrmConfigAsync),
+    IngredientsModule,
   ],
-  controllers: [],
-  providers: [],
 })
 export class AppModule {}
---
+++ server/src/ingredients/params.ts
@@ -0,0 +1,7 @@
+export type CreateIngredientParams = {
+  name: string;
+};
+
+export type UpdateIngredientParams = {
+  name: string;
+};
